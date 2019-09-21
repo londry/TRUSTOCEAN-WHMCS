5e6d517d50fe34b7e7be8a4909050ae6cd61fafc
Jason long <tanglongjun@qiaokr.com>
2019-09-21 23:03:37 +0800

    修复多个配置项导致ID不匹配san不扣费问题

--- modules/servers/TRUSTOCEANSSL/hooks.php
+++ modules/servers/TRUSTOCEANSSL/hooks.php
@@ -65,9 +65,16 @@ function trustoceanSslAddSanToCertOrderAndGetInvoice($vars){
         // $optionId = Capsule::table('tblhostingconfigoptions')->where('relid', $vars['serviceid'])->where('configid', $domaincount['id'])->value('optionid');
         $optionId = $domaincount["id"];
 
+        // 修复多个配置项导致ID不匹配不扣费问题
+        // 获取绝对的配置项价格configoptions的tblpricing.relid 对应  tblproductconfigoptionssub.id
+        $relIdforConfigoptionPricingCheck = Capsule::table('tblproductconfigoptionssub')
+            ->where('configid', $optionId)->where('optionname','LIKE','DomainCount%')
+            ->first();
+        $relIdforConfigoptionPricing = $relIdforConfigoptionPricingCheck->id;
+
         // 获取period内的单个SAN价格
         $netPrice = Capsule::table('tblpricing')->where('type','configoptions')
-            ->where('relid', $optionId)
+            ->where('relid', $relIdforConfigoptionPricing)
             ->where('currency', $whmcsClient->currency)
             ->value(strtolower($hosting['billingcycle']));
 
@@ -273,4 +280,3 @@ add_hook('InvoicePaid', 1, function($vars) {
         );
     }
 });
-
